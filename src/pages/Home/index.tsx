cb774b4d1ec292fedee4c87a06df30731414e4d1
Salik Imtiaz <salik.imtiaz@code-huddle.com>
2024-11-19 19:04:28 +0500

    coming soon texted added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import Layout from 'components/Layout'
+import React from 'react'
 
-function Home() {
+const Home: React.FC = () => {
     return (
         <Layout>
-            <div className="font-Jost flex min-h-screen items-center justify-center bg-secondary text-2xl">
-                Home Page
+            <div className="font-Jost flex min-h-screen items-center justify-center bg-gray-800 text-2xl">
+                <p className="text-center">
+                    <b className="text-white block">
+                        React-ShiftKit Documentation
+                    </b>
+                    🚀 Smooth shifts made simpler—comprehensive guides coming
+                    soon! Stay tuned. 🛠️
+                </p>
             </div>
         </Layout>
     )
